2ae11e9003d0abe17b297fe6a941e8e70da91282
Kael0595 <ants7021@naver.com>
2023-08-01 11:50:45 +0900

    answer 2개ììì2개씩 등록되도록 변경

--- src/main/java/com/sbb/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/sbb/exam/sbb/answer/AnswerController.java
@@ -32,14 +32,13 @@ public class AnswerController {
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model, @PathVariable("id") Integer id,
-               @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
+                               @Valid AnswerForm answerForm, BindingResult bindingResult, Principal principal) {
         Question question = this.questionService.getQuestion(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
         Answer answer = this.answerService.create(question,
                 answerForm.getContent(), siteUser);
         return String.format("redirect:/question/detail/%s#answer_%s",
